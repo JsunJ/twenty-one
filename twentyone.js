354f48c2616ebaae7ac8a86cd40f7f13276ebe25
Jason Jones <JsunJ.Jones@gmail.com>
2023-04-12 21:27:24 -0400

    add session data store, game initialization, bet page

--- twentyone.js
+++ twentyone.js
@@ -1,16 +1,39 @@
 const express = require("express");
 const morgan = require("morgan");
+const session = require("express-session");
+const store = require("connect-loki");
+const SessionPersistence = require("./lib/session-persistence");
 
 const app = express();
 const host = "localhost";
 const port = 3001;
+const LokiStore = store(session);
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(morgan("common"));
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+  cookie: {
+    httpOnly: true,
+    maxAge: 31 * 24 * 60 * 60 * 1000, // 31 days in millseconds
+    path: "/",
+    secure: false,
+  },
+  name: "twenty-one-game-session-id",
+  resave: false,
+  saveUninitialized: true,
+  secret: "insecure-please-change-me-later",
+  store: new LokiStore({}),
+}));
 
+// Create a new session datastore
+app.use((req, res, next) => {
+  res.locals.store = new SessionPersistence(req.session);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.redirect("/game/start");
@@ -18,26 +41,37 @@ app.get("/", (req, res) => {
 
 // Render game start page
 app.get("/game/start", (req, res) => {
+  res.locals.store.sessionLog();
   res.render("start");
 });
 
 // Start new game
-app.post("/game/new", (req, res) => {
-  // initalize players and hands to session store
-  // game in progress = true
+app.post("/game/new", (req, res, next) => {
+  let initialized = res.locals.store.initializeGame();
+  if (!initialized) {
+    next(new Error("Failed to initialize game."));
+  }
 
   res.redirect("/game/bet");
 });
 
 // Render the game bet page
 app.get("/game/bet", (req, res) => {
-  res.render("bet");
+  res.locals.store.sessionLog();
+  res.render("bet", {
+    // pass in player's current purse (read from pg store)
+  });
 });
 
 // Handle bet submission
-app.post("/game/post", (req, res) => {
-  // save bet to session store
+app.post("/game/bet", (req, res, next) => {
+  let betAmount = req.body.betAmount;
+  let placed = res.locals.store.placeBet(+betAmount);
+  if (!placed) {
+    next(new Error("Failed to place bet."));
+  }
 
+  res.locals.store.sessionLog();
   res.redirect("/game/player/turn");
 });
 
@@ -116,6 +150,12 @@ app.get("/game/new", (req, res) => {
   // QUIT to /game/start GET
 
   res.render("new");
+});
+
+// Error handler
+app.use((err, req, res, _next) => {
+  console.log(err);
+  res.status(404).send(err.message);
 });
 
 // Listener

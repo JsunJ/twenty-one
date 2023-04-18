5fb51216d30eb69ba76f8e1abc4a56708ddac2ef
Jason Jones <JsunJ.Jones@gmail.com>
2023-04-18 12:26:59 -0400

    add access restrictions for routes, dealer stand route

--- twentyone.js
+++ twentyone.js
@@ -56,6 +56,64 @@ app.use((req, res, next) => {
   next();
 });
 
+// Detect unauthorized access to routes.
+const requiresAuthentication = (req, res, next) => {
+  if (!res.locals.signedIn) {
+    console.log("Unauthorized.");
+    res.status(401).send("Unauthorized.");
+  } else {
+    next();
+  }
+};
+
+// Detect access to game routes without an existing game.
+const requiresInProgress = (req, res, next) => {
+  if (!res.locals.inProgress) {
+    console.log("Game not in progress.");
+    req.flash("error", "No game found. Please start a new game.");
+    res.redirect("/game/start");
+  } else {
+    next();
+  }
+};
+
+// Detect out-of-order access to game routes.
+const requiresCorrectGameFlow = (req, res, next) => {
+  let turn = res.locals.sessionStore.loadGameData().turn;
+  let path = req.route.path;
+
+  // refactor this
+
+  if (turn === "bet" && path !== "/game/bet") {
+    req.flash("error", "No bet found. Please place your bet.");
+    return res.redirect("/game/bet");
+  }
+
+  if (turn === "player" &&
+      path !== "/game/player/turn" &&
+      path !== "/game/deal" &&
+      path !== "/game/player/hit" &&
+      path !== "/game/player/stand") {
+    req.flash("error", "It is your turn to play.");
+    return res.redirect("/game/player/turn");
+  }
+
+  if (turn === "dealer" &&
+      path !== "/game/dealer/turn" &&
+      path !== "/game/dealer/hit" &&
+      path !== "/game/dealer/stand") {
+    req.flash("error", "It is the dealer's turn to play.");
+    return res.redirect("/game/dealer/turn");
+  }
+
+  if (turn === "over" && path !== "/game/over") {
+    req.flash("error", "The current game has ended.");
+    return res.redirect("/game/over");
+  }
+
+  return next();
+};
+
 app.get("/", (req, res) => {
   res.redirect("/game/start");
 });
@@ -72,185 +130,223 @@ app.post("/game/new", (req, res, next) => {
     next(new Error("Failed to initialize game."));
   }
   req.session.inProgress = true;
+  res.locals.sessionStore.setBetTurn();
 
   res.redirect("/game/bet");
 });
 
 // Render the game bet page
-app.get("/game/bet", (req, res) => {
-  // requiresInProgress
-  // if bet already placed, flash a message and
-  // CONTINUE to the current turn
-  res.render("bet");
-});
+app.get("/game/bet",
+  requiresInProgress,
+  requiresCorrectGameFlow,
+  (req, res, next) => {
+    res.render("bet");
+  }
+);
 
 // Handle bet submission
-app.post("/game/bet", (req, res, next) => {
-  // requiresInProgress
-  // if bet already placed, do nothing?
-  let betAmount = req.body.betAmount;
-  let placed = res.locals.sessionStore.placeBet(+betAmount);
-  if (!placed) {
-    next(new Error("Failed to place bet."));
-  }
-
-  req.flash("gameplay", "Bet placed!");
-  res.locals.sessionStore.setPlayerTurn();
-  res.redirect("/game/player/turn");
-});
-
-// Render the player turn page
-app.get("/game/player/turn", (req, res) => {
-  // requiresInProgress
-  let gameData = res.locals.sessionStore.loadGameData();
-
-  if (res.locals.sessionStore.isBusted(gameData.player)) {
-    res.redirect("/game/over");
-  }
-
-  res.render("player-turn", {
-    dealer: gameData.dealer,
-    player: gameData.player,
-    bet: gameData.bet,
-    handsEmpty: res.locals.sessionStore.handsEmpty(),
-    cardHidden: res.locals.sessionStore.isCardHidden(),
-    handValues: {
-      dealer: res.locals.sessionStore.getHandValue(gameData.dealer),
-      player: res.locals.sessionStore.getHandValue(gameData.player),
-    }
-  });
-});
-
-// Deal out player and dealer hands
-app.post("/game/deal", (req, res, next) => {
-  // requiresInProgress
-  res.locals.sessionStore.dealHands(); // error?
-  let hidden = res.locals.sessionStore.hideCard();
-  if (!hidden) {
-    next(new Error("Failed to hide card."));
-  }
-
-  if (res.locals.sessionStore.hasBlackJack()) {
-    res.locals.sessionStore.setBlackJack();
-    let revealed = res.locals.sessionStore.revealCard();
-    if (!revealed) {
-      next(new Error("Failed to reveal card."));
+app.post("/game/bet",
+  requiresInProgress,
+  requiresCorrectGameFlow,
+  (req, res, next) => {
+    // if bet already placed, do nothing?
+    let betAmount = req.body.betAmount;
+    let placed = res.locals.sessionStore.placeBet(+betAmount);
+    if (!placed) {
+      next(new Error("Failed to place bet."));
     }
 
-    res.redirect("/game/over");
-  }
-
-  res.redirect("/game/player/turn");
-});
-
-// Handle player hit
-app.post("/game/player/hit", (req, res, next) => {
-  // requiresInProgress
-  let dealt = res.locals.sessionStore.dealPlayerCard();
-  if (!dealt) {
-    next(new Error("Failed to deal card."));
-  }
-
-  req.flash("gameplay", "Hit!");
-
-  let player = res.locals.sessionStore.loadGameData().player;
-  if (res.locals.sessionStore.isBusted(player)) {
-    res.locals.sessionStore.bustPlayer(player);
-    let revealed = res.locals.sessionStore.revealCard();
-    if (!revealed) {
-      next(new Error("Failed to reveal card."));
-    }
-    res.redirect("/game/over");
-  } else {
+    req.flash("gameplay", "Bet placed!");
+    res.locals.sessionStore.setPlayerTurn();
     res.redirect("/game/player/turn");
   }
-});
+);
+
+// Render the player turn page
+app.get("/game/player/turn",
+  requiresInProgress,
+  requiresCorrectGameFlow,
+  (req, res) => {
+    let gameData = res.locals.sessionStore.loadGameData();
+
+    if (res.locals.sessionStore.isBusted(gameData.player)) {
+      res.locals.sessionStore.setGameOverTurn();
+      res.redirect("/game/over");
+    }
+
+    res.render("player-turn", {
+      dealer: gameData.dealer,
+      player: gameData.player,
+      bet: gameData.bet,
+      handsEmpty: res.locals.sessionStore.handsEmpty(),
+      cardHidden: res.locals.sessionStore.isCardHidden(),
+      handValues: {
+        dealer: res.locals.sessionStore.getHandValue(gameData.dealer),
+        player: res.locals.sessionStore.getHandValue(gameData.player),
+      }
+    });
+  }
+);
+
+// Deal out player and dealer hands
+app.post("/game/deal",
+  requiresInProgress,
+  requiresCorrectGameFlow,
+  (req, res, next) => {
+    res.locals.sessionStore.dealHands(); // error?
+    let hidden = res.locals.sessionStore.hideCard();
+    if (!hidden) {
+      next(new Error("Failed to hide card."));
+    }
+
+    if (res.locals.sessionStore.hasBlackJack()) {
+      res.locals.sessionStore.setBlackJack();
+      res.locals.sessionStore.setGameOverTurn();
+      let revealed = res.locals.sessionStore.revealCard();
+      if (!revealed) {
+        next(new Error("Failed to reveal card."));
+      }
+
+      res.redirect("/game/over");
+    }
+
+    res.redirect("/game/player/turn");
+  }
+);
+
+// Handle player hit
+app.post("/game/player/hit",
+  requiresInProgress,
+  requiresCorrectGameFlow,
+  (req, res, next) => {
+    let dealt = res.locals.sessionStore.dealPlayerCard();
+    if (!dealt) {
+      next(new Error("Failed to deal card."));
+    }
+
+    req.flash("gameplay", "Hit!");
+
+    let player = res.locals.sessionStore.loadGameData().player;
+    if (res.locals.sessionStore.isBusted(player)) {
+      res.locals.sessionStore.bustPlayer(player);
+      res.locals.sessionStore.setGameOverTurn();
+      let revealed = res.locals.sessionStore.revealCard();
+      if (!revealed) {
+        next(new Error("Failed to reveal card."));
+      }
+      res.redirect("/game/over");
+    } else {
+      res.redirect("/game/player/turn");
+    }
+  }
+);
 
 // Handle player stand
-app.post("/game/player/stand", (req, res, next) => {
-  // requiresInProgress
-  res.locals.sessionStore.setDealerTurn();
-  let revealed = res.locals.sessionStore.revealCard();
-  if (!revealed) {
-    next(new Error("Failed to reveal card."));
-  }
+app.post("/game/player/stand",
+  requiresInProgress,
+  requiresCorrectGameFlow,
+  (req, res, next) => {
+    res.locals.sessionStore.setDealerTurn();
+    let revealed = res.locals.sessionStore.revealCard();
+    if (!revealed) {
+      next(new Error("Failed to reveal card."));
+    }
 
-  req.flash("gameplay", "Stand!");
-  req.flash("gameplay", "The dealer revealed their facedown card!");
+    req.flash("gameplay", "Stand!");
+    req.flash("gameplay", "The dealer revealed their facedown card!");
 
-  res.redirect("/game/dealer/turn");
-});
-
-// Render the dealer turn page
-app.get("/game/dealer/turn", (req, res) => {
-  // requiresInProgress
-  let gameData = res.locals.sessionStore.loadGameData();
-
-  // if (res.locals.sessionStore.isBusted(gameData.dealer)) {
-  //   res.redirect("/game/over");
-  // }
-
-  res.render("dealer-turn", {
-    dealer: gameData.dealer,
-    player: gameData.player,
-    bet: gameData.bet,
-    cardHidden: res.locals.sessionStore.isCardHidden(),
-    handValues: {
-      dealer: res.locals.sessionStore.getHandValue(gameData.dealer),
-      player: res.locals.sessionStore.getHandValue(gameData.player),
-    },
-  });
-});
-
-// Handle dealer hit
-app.post("/game/dealer/hit", (req, res, next) => {
-  // requiresInProgress
-  let dealt = res.locals.sessionStore.dealDealerCard();
-  if (!dealt) {
-    next(new Error("Failed to deal card."));
-  }
-
-  req.flash("gameplay", "The dealer hits!");
-
-  let dealer = res.locals.sessionStore.loadGameData().dealer;
-  if (res.locals.sessionStore.isBusted(dealer)) {
-    res.locals.sessionStore.bustPlayer(dealer);
-    res.redirect("/game/over");
-  } else {
     res.redirect("/game/dealer/turn");
   }
-});
+);
+
+// Render the dealer turn page
+app.get("/game/dealer/turn",
+  requiresInProgress,
+  requiresCorrectGameFlow,
+  (req, res) => {
+    let gameData = res.locals.sessionStore.loadGameData();
+
+    if (res.locals.sessionStore.isBusted(gameData.dealer)) {
+      res.locals.sessionStore.setGameOverTurn();
+      res.redirect("/game/over");
+    }
+
+    res.render("dealer-turn", {
+      dealer: gameData.dealer,
+      player: gameData.player,
+      bet: gameData.bet,
+      cardHidden: res.locals.sessionStore.isCardHidden(),
+      handValues: {
+        dealer: res.locals.sessionStore.getHandValue(gameData.dealer),
+        player: res.locals.sessionStore.getHandValue(gameData.player),
+      },
+    });
+  }
+);
+
+// Handle dealer hit
+app.post("/game/dealer/hit",
+  requiresInProgress,
+  requiresCorrectGameFlow,
+  (req, res, next) => {
+    let dealt = res.locals.sessionStore.dealDealerCard();
+    if (!dealt) {
+      next(new Error("Failed to deal card."));
+    }
+
+    req.flash("gameplay", "The dealer hits!");
+
+    let dealer = res.locals.sessionStore.loadGameData().dealer;
+    if (res.locals.sessionStore.isBusted(dealer)) {
+      res.locals.sessionStore.bustPlayer(dealer);
+      res.locals.sessionStore.setGameOverTurn();
+      res.redirect("/game/over");
+    } else {
+      res.redirect("/game/dealer/turn");
+    }
+  }
+);
+
+// Handle dealer stand
+app.post("/game/dealer/stand",
+  requiresInProgress,
+  requiresCorrectGameFlow,
+  (req, res) => {
+    res.locals.sessionStore.setGameOverTurn();
+    req.flash("gameplay", "The dealer stands!");
+    res.redirect("/game/over");
+  }
+);
 
 // Render the game over page
-app.get("/game/over", (req, res) => {
-  // requiresInProgress
-  let gameData = res.locals.sessionStore.loadGameData();
+app.get("/game/over",
+  requiresInProgress,
+  requiresCorrectGameFlow,
+  (req, res) => {
+    let gameData = res.locals.sessionStore.loadGameData();
 
-  if (res.locals.sessionStore.getHandValue(gameData.dealer) >= 17 &&
-      !gameData.dealer.isBusted && !gameData.player.isBusted) {
-    res.locals.flash.gameplay = [ "The dealer stands!" ];
+    res.render("over", {
+      dealer: gameData.dealer,
+      player: gameData.player,
+      bet: gameData.bet,
+      winner: res.locals.sessionStore.determineWinner(),
+      cardHidden: res.locals.sessionStore.isCardHidden(),
+      handValues: {
+        dealer: res.locals.sessionStore.getHandValue(gameData.dealer),
+        player: res.locals.sessionStore.getHandValue(gameData.player),
+      },
+      hasBlackJack: gameData.player.hasBlackJack,
+    });
   }
-
-  res.render("over", {
-    dealer: gameData.dealer,
-    player: gameData.player,
-    bet: gameData.bet,
-    winner: res.locals.sessionStore.determineWinner(),
-    cardHidden: res.locals.sessionStore.isCardHidden(),
-    handValues: {
-      dealer: res.locals.sessionStore.getHandValue(gameData.dealer),
-      player: res.locals.sessionStore.getHandValue(gameData.player),
-    },
-    hasBlackJack: gameData.player.hasBlackJack,
-  });
-});
+);
 
 // Handle payouts and collections
 app.post("/game/over",
+  requiresInProgress,
+  requiresCorrectGameFlow,
   catchError(async (req, res) => {
-    // requiresInProgress
 
-    // make all this better
+    // refactor this
     if (res.locals.signedIn) {
       let winner = res.locals.sessionStore.determineWinner();
       let bet = res.locals.sessionStore.loadGameData().bet;
@@ -284,11 +380,11 @@ app.post("/game/over",
 );
 
 // Render the play again page
-app.get("/game/new", (req, res) => {
-  // requiresInProgress
-
-  res.render("new");
-});
+app.get("/game/new",
+  (req, res) => {
+    res.render("new");
+  }
+);
 
 // Render the sign in page
 app.get("/users/signin", (req, res) => {
@@ -336,6 +432,7 @@ app.post("/users/signout", (req, res) => {
 
 // Render the leaderboard page
 app.get("/game/leaderboard",
+  requiresAuthentication,
   catchError(async (req, res) => {
     let stats = await res.locals.pgStore.leaderboard();
 
